830ac0072496db0c8bc397f4796c036630b28ab9
juansec04 <jramir62@charlotte.edu>
2026-04-01 01:28:38 -0400

    Create website_evaluations.jsx

---
+++ itis3135-react/src/website_evaluations.jsx
@@ -0,0 +1,67 @@
+export default function WebsiteEvaluations() {
+    return (
+        <main>
+            <h2>Crap Principles Evaluations</h2>
+
+      <section>
+        <h3>
+          <a
+            href="https://www.apple.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Apple
+          </a>
+        </h3>
+        <ul>
+          <li>
+            Contrast: Excellent - bold headings, and clear visual hierarchy make
+            the content easy to read.
+          </li>
+          <li>
+            Repetition: Very consistent use of typography, color, and layout
+            styles across the site.
+          </li>
+          <li>
+            Alignment: Strong grid and alignment patterns make the layout
+            visually clean and easy to read.
+          </li>
+          <li>
+            Proximity: Content groups are spaced and separated logically for
+            easy scanning.
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>
+          <a
+            href="https://www.huawei.com/en/ipr?gad_source=1&gad_campaignid=23434758710&gbraid=0AAAABChKHZcGLePIcpcvGlDsyMQ2_TEsm&gclid=EAIaIQobChMIz_yZrJzakgMVQjbUAR1QRSzgEAAYASAAEgIsqvD_BwE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Huawei
+          </a>
+        </h3>
+        <ul>
+          <li>Contrast: Weak emphasis, too many items compete for attention.</li>
+          <li>
+            Repetition: Inconsistent, feeling like some sections look like
+            marketing blocks while others like a report, so the page feels
+            stitched together instead of consistent.
+          </li>
+          <li>
+            Alignment: Not terrible, but visually there are many stacked menus
+            and sections and the show more section make the layout feel heavy
+            rather than clean.
+          </li>
+          <li>
+            Proximity: Crowded and overwhelming too, many links and categories
+            are packed in the top area, and long blocks of text make it harder
+            to quickly find what you need.
+          </li>
+        </ul>
+      </section>
+        </main>
+    );
+}
