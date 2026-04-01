143796aef792c7f3a65b25dee49875686cc9572e
juansec04 <jramir62@charlotte.edu>
2026-04-01 01:28:21 -0400

    Create App.jsx

---
+++ itis3135-react/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h2>Course Home</h2>
+        <p>
+          Welcome to my ITIS 3135 course page. This site showcases my work and
+          progress throughout the semester.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
