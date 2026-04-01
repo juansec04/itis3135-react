959fe34e50258e226c4ebe2f949280d8f8785890
juansec04 <jramir62@charlotte.edu>
2026-04-01 01:29:06 -0400

    Create footer.jsx

---
+++ itis3135-react/src/footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/juansec04" target="_blank" rel="noopener noreferrer">GitHub</a> |{" "}
+            <a href="https://juansec04.github.io" target="_blank" rel="noopener noreferrer">GitHub.io</a> |{" "}
+            <a href="https://webpages.charlotte.edu/jramir62" target="_blank" rel="noopener noreferrer">CLT Web</a> |{" "}
+            <a href="https://www.freecodecamp.org/juansec04" target="_blank" rel="noopener noreferrer">freeCodeCamp</a> |{" "}
+            <a href="https://www.linkedin.com/in/juan-ramirez-982ba7385/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+
+      <p>Copyright 2026 &copy; Juan Ramirez</p>
+      <p>Page created by Juan Ramirez Designs</p>
+
+      <p>
+        Certified in{" "}
+        <a
+          href="https://www.freecodecamp.org/certification/jdrc/responsive-web-design"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Responsive Web Design
+        </a>
+      </p>
+        </footer>
+    );
+}
